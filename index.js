4a657e4237f81d7d0872e08da882ffc4e41132f2
Grace <112779767+grachimus@users.noreply.github.com>
2022-12-05 16:49:06 -0600

    User input and creating employee objects complete

--- index.js
+++ index.js
@@ -1,10 +1,67 @@
 const { finalize } = require('rxjs');
 const App = require('./lib/App')
+const inquirer = require('inquirer');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
 
+const {mngQuestions, menuQuestion, engQuestions, intQuestions} = require('./lib/Questions');
 //Initialize new object for App
-const app = new App()
+const app = new App();
 
 //Start the app
-app.start();
+app.mngPrompt();
+// let rosterArray = [];
 
+// function mngPrompt() {
+//     inquirer
+//         .prompt(mngQuestions)
+//         .then((data) => {
+//             const manager = new Manager( data.mngName, data.mngId, data.mngEmail, data.office );
+//             rosterArray.push(manager);
+//             console.log(rosterArray);
+//             intEngPrompt();
+//         })
+//     }
 
+// function intEngPrompt() {
+//         inquirer
+//             .prompt(menuQuestion)
+//             .then((data) => {
+//                 switch (data.menu) {
+//                     case 'Add an engineer to the team':
+//                         this.engPrompt();
+//                         // this.menuPrompt();
+//                         break;
+//                     case  'Add an intern to the team':
+//                         this.intPrompt();
+//                         // this.menuPrompt();
+//                         break;
+//                     case "Finalize team and generate roster":
+//                         this.finalize()
+//                         break;
+//                 }
+//             })
+//     }
+
+// function engPrompt() {
+//         inquirer
+//         .prompt(engQuestions)
+//         .then((data) => {
+//             const engineer = new Engineer( data.engName, data.engId, data.engEmail, data.engGithub );
+//             this.rosterArray.push(engineer);
+//             console.log(this.rosterArray);
+//         })
+//     }
+
+// function intPrompt() {
+//         inquirer
+//         .prompt(intQuestions)
+//         .then((data) => {
+//             const intern = new Intern( data.intName, data.intId, data.intEmail, data.intSchool );
+//             this.rosterArray.push(intern);
+//             console.log(this.rosterArray);
+//         })
+//     }
+
+// mngPrompt();
---
+++ src/generateHTML.js
@@ -0,0 +1,15 @@
+// Template goes here
+// Loop through your team array
+
+function createHTML() {
+// loop through roster and push each object to the template to its type
+    loop (rosterArray) {
+    if roster[i] === intern {
+        createIntern()
+
+    }
+// after all respective templates are completed, append to the main generateHTML.js template
+}
+}
+// Each type of employee will have their own template
+function createEngineer()
