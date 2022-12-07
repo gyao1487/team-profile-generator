d67860f69284e959c21974760936d92e6b315b20
Grace <112779767+grachimus@users.noreply.github.com>
2022-12-06 19:12:02 -0600

    working on finalize function

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -1,15 +1,31 @@
+//Dependencies
+
 // Template goes here
+
+
+
+
 // Loop through your team array
 
-function createHTML() {
-// loop through roster and push each object to the template to its type
-    loop (rosterArray) {
-    if roster[i] === intern {
-        createIntern()
-
+function generateHTML() {
+    // loop through roster and push each object to the template to its type
+    for (let i = 0; i < app.rosterArray.length; i++) {
+        switch (app.rosterArray[i]) {
+            case 'Manager':
+                console.log('This is a Manager!')
+                break;
+            case 'Engineer':
+                console.log('This is an Engineer')
+                break;
+            case 'Intern':
+                console.log('This is an employee')
+                break;
+        }
     }
+};
 // after all respective templates are completed, append to the main generateHTML.js template
-}
-}
+
 // Each type of employee will have their own template
-function createEngineer()
+//function createEngineer()
+
+module.exports = generateHTML
