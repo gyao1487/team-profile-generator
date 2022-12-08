4e589530d909dcbad0b5fe7a5743f07fc66f6cb3
Grace <112779767+grachimus@users.noreply.github.com>
2022-12-07 21:34:01 -0600

    jest dependency fixed

--- index.js
+++ index.js
@@ -1,27 +1,17 @@
-const { finalize } = require('rxjs');
 const App = require('./lib/App')
-const inquirer = require('inquirer');
-const Manager = require('./lib/Manager');
-const Engineer = require('./lib/Engineer');
-const Intern = require('./lib/Intern');
-
+const util = require('util')
 const {mngQuestions, menuQuestion, engQuestions, intQuestions} = require('./lib/Questions');
+
 //Initialize new object for App
 const app = new App();
 
-
-
-//Start the app by starting the Manager prompts
+// Function to start app
 async function init() {
     try {
        await app.mngPrompt();
-       //function for writing HTML
-    //    window.open('http://localhost:52330/dist/index.html');
     } catch (err) {
         console.log("Sorry, we've encountered an error!")
     }
 }
 
-
-// By the end of this function, you will have generated markdown 
 init();
